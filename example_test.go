c874881987b3155116b40d80ba9cf37d05ea5512
Gregory Vincic <gregoryvse@gmail.com>
2023-11-17 22:07:01 +0100

    Update example

--- example_test.go
+++ example_test.go
@@ -8,13 +8,12 @@ import (
 )
 
 func ExampleDuration() {
-	d := uptime.Between(
-		time.Date(2021, 1, 01, 12, 00, 00, 0, time.UTC),
-		time.Date(2022, 3, 02, 13, 10, 20, 0, time.UTC),
-	)
+	a := time.Date(1022, 1, 01, 12, 00, 00, 0, time.UTC)
+	b := time.Date(2022, 3, 02, 13, 10, 20, 0, time.UTC)
+	d := uptime.Between(a, b)
 	fmt.Println(d.String())
 	fmt.Println(d.Short())
 	// output:
-	// 1 year 2 months 1 day 1 hour 10 minutes 20 seconds
-	// 1y2m1d 1h10m20s
+	// 1000 years 2 months 1 day 1 hour 10 minutes 20 seconds
+	// 1000y2m1d 1h10m20s
 }
